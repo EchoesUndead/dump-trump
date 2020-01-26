915adbf1ea5abadd8394a2a35efbca043347037d
EchoesUndead <mrlonis@outlook.com>
2020-01-26 17:48:32 -0500

    Create Update Index.ps1

---
+++ scripts/Update Index.ps1
@@ -0,0 +1,4 @@
+. C:\GitHub\EchoesUndead\dump-trump\scripts\Functions\Update-MarkDownIndex.ps1
+
+Update-MarkDownIndex -FilePath 'C:\GitHub\EchoesUndead\dump-trump\Trump''s Record on the Environment.md' -Verbose
+
